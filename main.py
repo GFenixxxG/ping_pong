18f2d4b7f61c6f10fb15e043a41885e68ec87872
Fenixxx <ehorpython@gmail.com>
2023-09-01 19:55:52 +0300

    version 3

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+#Підключення біблеотек
 from pygame import *
-
+#Ініацалізація всіх процесів
 init()
-
+#Розміри екрану
 W = 800
 H = 500
-
+#Задній фон
 back = (0, 255, 0)
-
+#Екран
 window = display.set_mode((W, H))
 display.set_icon(image.load("tenis_ball.png"))
 display.set_caption("PING PONG")
 window.fill(back)
-
+#Базовий класс для обєктів
 class GameSprite(sprite.Sprite):
     # конструктор класу
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -23,11 +24,12 @@ class GameSprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
-
+    #Функція для відмальовування
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
-
+#Класс для гравця
 class Player(GameSprite):
+    #Функція для руху гравця L
     def update_l(self):
         keys_pressed = key.get_pressed()
 
@@ -36,7 +38,7 @@ class Player(GameSprite):
 
         if keys_pressed[K_s] and self.rect.y < H - self.size_y:
             self.rect.y += self.speed
-
+    #Функція для руху гравця R
     def update_r(self):
         keys_pressed = key.get_pressed()
 
@@ -46,16 +48,13 @@ class Player(GameSprite):
         if keys_pressed[K_DOWN] and self.rect.y < H - self.size_y:
             self.rect.y += self.speed
 
-
+#Гравці
 player_l = Player('racket.png', 10, W/3, 50, 150, 5)
-
 player_r = Player('racket.png', 740, W/3, 50, 150, 5)
-
-
+#Змінні для гри
 finish = False
 game = True
-
-
+#Цикл гри
 while game:
     time.delay(5)
 
@@ -67,10 +66,10 @@ while game:
     player_r.reset()
     player_r.update_r()
     
-
+    #Перевірка закриття гри
     for e in event.get():
         if e.type == QUIT:
             game = False
-            
+    #Оновлення екрану
     display.update()
 
