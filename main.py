7e1cc8589c3ea4d046592e154764494baf6c7742
Fenixxx <ehorpython@gmail.com>
2023-09-01 21:30:09 +0300

    version 5

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ init()
 #Розміри екрану
 W = 800
 H = 500
+#
+player_l_point = 0
+player_r_point = 0
+#
+speed_x = 1
+speed_y = 1
 #Задній фон
 back = (0, 255, 100)
 #Екран
@@ -12,6 +18,14 @@ window = display.set_mode((W, H))
 display.set_icon(image.load("tenis_ball.png"))
 display.set_caption("PING PONG")
 window.fill(back)
+"""FONTS"""
+
+#Ініціалізація шрифту
+font.init()
+#Шрифти
+font = font.SysFont("Arial", 50)
+
+
 #Базовий класс для обєктів
 class GameSprite(sprite.Sprite):
     # конструктор класу
@@ -48,28 +62,61 @@ class Player(GameSprite):
         if keys_pressed[K_DOWN] and self.rect.y < H - self.size_y:
             self.rect.y += self.speed
 
-#Гравці
+#Гравці     
 player_l = Player('racket.png', 10, W/3, 50, 150, 5)
 player_r = Player('racket.png', 740, W/3, 50, 150, 5)
+
+ball = GameSprite('tenis_ball.png', 200, 200, 75, 75, 1)
+
 #Змінні для гри
 finish = False
 game = True
 #Цикл гри
 while game:
-    time.delay(5)
-
-    window.fill(back)
-
-    player_l.reset()
-    player_l.update_l() 
-
-    player_r.reset()
-    player_r.update_r()
-    
     #Перевірка закриття гри
     for e in event.get():
         if e.type == QUIT:
             game = False
-    #Оновлення екрану
-    display.update()
+
+    window.fill(back)
+
+    point_l_txt = font.render(str(player_l_point), 1, (0, 0, 0))
+    point_r_txt = font.render(str(player_r_point), 1, (0, 0, 0))
+
+    window.blit(point_l_txt, (725, 25))
+    window.blit(point_r_txt, (54, 25))
+
+    if not finish:
+        time.delay(5)
+
+        player_l.reset()
+        player_l.update_l() 
+
+        player_r.reset()
+        player_r.update_r()
+
+        ball.reset()
+
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+            
+        if ball.rect.y < 0 or ball.rect.y > H - 75:    
+            speed_y *= -1
+
+        if sprite.collide_rect(player_l, ball) or sprite.collide_rect(player_r, ball):
+            speed_x *= -1
+
+        if ball.rect.x > W - 75:    
+            player_l_point += 1
+            finish == True
+            ball.rect.x = 200
+            ball.rect.y = 200
+
+        if ball.rect.x < 0: 
+            player_r_point += 1
+            finish == True
+            ball.rect.x = 200
+            ball.rect.y = 200
+        #Оновлення екрану
+        display.update()
 
